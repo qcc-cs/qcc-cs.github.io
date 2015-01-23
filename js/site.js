4a196df5dfa3458663b7a0ee4c40ac84f8a470ae
kwangkim <kkim@qcc.cuny.edu>
2015-01-22 22:49:45 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -72,7 +72,7 @@ $.extend($.easing,
 
 $(document).ready(function (){
 
-    $('navscroll nav li a').navScroller();
+    $(' nav li a').navScroller();
 
     //section divider icon click gently scrolls to reveal the section
 	$(".sectiondivider").on('click', function(event) {
