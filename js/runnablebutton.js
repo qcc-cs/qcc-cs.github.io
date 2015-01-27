5057299618ea43122d2e8fab3edef11a012574c2
kwangkim <kkim@qcc.cuny.edu>
2015-01-27 16:50:46 -0500

    Create runnablebutton.js

---
+++ js/runnablebutton.js
@@ -0,0 +1,35 @@
+var Trybutton = React.createClass({
+  render: function(){
+    return (
+      <div  classname="btn-try" class="btn-group" role="group">
+        <a  classname="try-link" class="btn btn-default bg-purple"  href="{this.props.tryurl}" target="_blank"> TRY FIRST.</a>
+      </div>
+    );
+  }
+});
+var Answerbutton = React.createClass({
+  render: function(){
+    return (
+      <div classname="btn-ans" class="btn-group" role="group">
+        <a  classname="ans-link" class="btn btn-default bg-blue"  href="{this.props.ansurl}" target="_blank"> SAMPLE ANSWER.</a>
+      </div>
+    );
+  }
+});
+var Rbutton =React.createClass({
+  render: function(){
+    return (
+      <div classname="btn-r" class="btn-group btn-group-justified" role="group" aria-label="...">
+        <Trybutton tryurl={this.props.tryurl}/>
+        <Answerbutton ansurl={this.pros.ansurl}/>
+      </div>
+    );
+  }
+});
+var makerbutton=function(urls){
+  for( i=0; i<urls.length;i++){
+    var mybutton=<Rbutton tryurl={ urls[i].try} ansurl={urls[i].ans} />;
+    React.render(mybutton,document.getElementById(urls[i].id));
+  }
+}
+// var url=[{try:" ",ans:"",id:" "},...];
