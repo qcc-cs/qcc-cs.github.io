62ac3c241929355f701b9b74701a38da20565722
kwangkim <kkim@qcc.cuny.edu>
2015-01-27 22:50:16 -0500

    Update rbutton.js

--- js/rbutton.js
+++ js/rbutton.js
@@ -1,18 +1,14 @@
 var Trybutton = React.createClass({
   render: function(){
     return (
-      <div  className="btn-group" role="group">
-        <a  classMame="btn btn-default bg-purple"   href={this.props.tryurl}  target="_blank"> TRY FIRST.</a>
-      </div>
+        <a  role="button" classMame="btn btn-default bg-purple"   href={this.props.tryurl}  target="_blank"> TRY FIRST.</a>
     );
   }
 });
 var Answerbutton = React.createClass({
   render: function(){
     return (
-      <div className="btn-group"  role="group">
-        <a  clasNname="btn btn-default bg-blue"   href={this.props.ansurl} target="_blank"> SAMPLE ANSWER.</a>
-      </div>
+        <a  role="button" clasNname="btn btn-default bg-blue"   href={this.props.ansurl} target="_blank"> SAMPLE ANSWER.</a>
     );
   }
 });
