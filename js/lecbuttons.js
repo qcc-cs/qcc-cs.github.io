af6cc1f56171fbcb8562a433d87540a883cfcd0b
kwangkim <kkim@qcc.cuny.edu>
2015-01-28 07:14:53 -0500

    Update lecbuttons.js

--- js/lecbuttons.js
+++ js/lecbuttons.js
@@ -14,10 +14,6 @@ var lecbuttons=[{
   "try": "VMiH5eFcLRURBt4-",
   "ans": "VMiMMVPay30V5EZQ",
   "id": "rbutton-1-4-1"
-},{
-  "try": "VMiH5eFcLRURBt4-",
-  "ans": "VMiMMVPay30V5EZQ",
-  "id": "rbutton-2-1-1"
 }
 ];
 
