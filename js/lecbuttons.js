7eb7cf2e9236e90777756896529e6f14dc43fe9a
kwangkim <kkim@qcc.cuny.edu>
2015-01-28 02:14:44 -0500

    Update lecbuttons.js

--- js/lecbuttons.js
+++ js/lecbuttons.js
@@ -10,5 +10,10 @@ var lecbuttons=[{
   "try": "VMgPIoFjlw9TxK4a",
   "ans": "VMgPuRaubcVGrlkv",
   "id": "rbutton-1-3-1"
-}];
+},{
+  "try": "VMiH5eFcLRURBt4-",
+  "ans": "VMiMMVPay30V5EZQ",
+  "id": "rbutton-1-4-1"
+}
+];
 
