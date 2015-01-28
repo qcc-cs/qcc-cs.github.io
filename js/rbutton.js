c2f8518779a3f5e81ee18beed2b26fe985d649de
kwangkim <kkim@qcc.cuny.edu>
2015-01-28 06:53:41 -0500

    Update rbutton.js

--- js/rbutton.js
+++ js/rbutton.js
@@ -8,7 +8,7 @@ var Trybutton = React.createClass({displayName: "Trybutton",
 var Answerbutton = React.createClass({displayName: "Answerbutton",
   render: function(){
     return (
-        React.createElement("a", {className: "btn btn-success pull-right", href: this.props.ansurl, target: "_blank"}, "ANSWER.")
+        React.createElement("a", {className: "btn btn-success pull-right", href: this.props.ansurl, target: "_blank"}, "ANSWER")
     );
   }
 });
@@ -23,13 +23,37 @@ var Rbutton =React.createClass({displayName: "Rbutton",
   }
 });
 function makerbutton(){ //http://stackoverflow.com/a/2223341
+  //rbutton-1-3-1
+  //http://stackoverflow.com/a/5616842
+  //http://stackoverflow.com/a/3239600
+  //http://stackoverflow.com/a/5181740
+  //http://www.sitepoint.com/jquery-each-examples/
+  //http://stackoverflow.com/a/7364307
   var urls=lecbuttons;
-  for(var i=0; i<urls.length;i++){
-    var tryurl="http://runnable.com/"+urls[i].try;
-    console.log("try:"+tryurl);
-    var ansurl="http://runnable.com/"+urls[i].ans;
-    console.log("ans:"+ansurl);
-    var mybutton=React.createElement(Rbutton, {tryurl: tryurl, ansurl: ansurl});
-    React.render(mybutton,document.getElementById(urls[i].id));
-  }
+  $('#my-pagination-content [id^="rbutton-"]').each(
+    function(index1, value1) {
+      myid = $(this).attr("id");
+      result = $.grep(lecbuttons, function(e) {
+        return e.id === myid;
+      });
+      if (result.length == 0) {
+        console.log("No match");
+      } else if (result.length == 1) {
+        var tryurl = "http://runnable.com/" + result.try;
+        console.log("try:" + tryurl);
+        var ansurl = "http://runnable.com/" + result.ans;
+        console.log("ans:" + ansurl);
+        var mybutton = React.createElement(Rbutton, {tryurl: 
+          tryurl, 
+        
+        ansurl: 
+          ansurl
+        }
+        );
+        React.render(mybutton, document.getElementById(result.id));
+      } else {
+        console.log("Mutiple matches.");
+      }
+    }
+  );
 }
