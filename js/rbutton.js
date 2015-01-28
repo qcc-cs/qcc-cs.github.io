aded047a402c2a76b9495083e2a0e7e95ab248f4
kwangkim <kkim@qcc.cuny.edu>
2015-01-27 22:43:36 -0500

    Update rbutton.js

--- js/rbutton.js
+++ js/rbutton.js
@@ -3,7 +3,7 @@ var Trybutton = React.createClass({
     return (
       <div  className="btn-group" role="group">
         <a  classMame="btn btn-default bg-purple"   href={this.props.tryurl}  target="_blank"> TRY FIRST.</a>
-      </div>&nbsp;
+      </div>
     );
   }
 });
@@ -12,7 +12,7 @@ var Answerbutton = React.createClass({
     return (
       <div className="btn-group"  role="group">
         <a  clasNname="btn btn-default bg-blue"   href={this.props.ansurl} target="_blank"> SAMPLE ANSWER.</a>
-      </div>&nbsp;
+      </div>
     );
   }
 });
