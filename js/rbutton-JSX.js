ec36144eee812d7b7322c305ba894aa76c619076
kwangkim <kkim@qcc.cuny.edu>
2015-01-28 01:06:18 -0500

    Update rbutton-JSX.js

--- js/rbutton-JSX.js
+++ js/rbutton-JSX.js
@@ -1,21 +1,21 @@
 var Trybutton = React.createClass({
   render: function(){
     return (
-        <a  role="button" classMame="btn btn-default bg-purple"   href={this.props.tryurl}  target="_blank"> TRY FIRST.</a>
+        <a  className="btn btn-info pull-left"   href={this.props.tryurl}  target="_blank"> TRY FIRST.</a>
     );
   }
 });
 var Answerbutton = React.createClass({
   render: function(){
     return (
-        <a  role="button" clasNname="btn btn-default bg-blue"   href={this.props.ansurl} target="_blank"> SAMPLE ANSWER.</a>
+        <a   className="btn btn-success pull-right"   href={this.props.ansurl} target="_blank"> SAMPLE ANSWER.</a>
     );
   }
 });
 var Rbutton =React.createClass({
   render: function(){
     return (
-      <div className="btn-group btn-group-justified" role="group" aria-label="...">
+      <div className="text-center container">
         <Trybutton tryurl={this.props.tryurl}/>
         <Answerbutton ansurl={this.props.ansurl}/>
       </div>
