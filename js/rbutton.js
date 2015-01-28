d45dd1d45b559cde2d993d98bca96715b5f6199b
kwangkim <kkim@qcc.cuny.edu>
2015-01-27 21:43:26 -0500

    Update rbutton.js

--- js/rbutton.js
+++ js/rbutton.js
@@ -1,8 +1,8 @@
 var Trybutton = React.createClass({
   render: function(){
     return (
-      <div  className="btn-try" class="btn-group" role="group">
-        <a  classMame="try-link" class="btn btn-default bg-purple"  href="{this.props.tryurl}" target="_blank"> TRY FIRST.</a>
+      <div  className="btn-group" role="group">
+        <a  classMame="btn btn-default bg-purple"   href="{this.props.tryurl}" target="_blank"> TRY FIRST.</a>
       </div>
     );
   }
@@ -10,8 +10,8 @@ var Trybutton = React.createClass({
 var Answerbutton = React.createClass({
   render: function(){
     return (
-      <div className="btn-ans" class="btn-group" role="group">
-        <a  clasNname="ans-link" class="btn btn-default bg-blue"  href="{this.props.ansurl}" target="_blank"> SAMPLE ANSWER.</a>
+      <div className="btn-group"  role="group">
+        <a  clasNname="btn btn-default bg-blue"   href="{this.props.ansurl}" target="_blank"> SAMPLE ANSWER.</a>
       </div>
     );
   }
@@ -19,7 +19,7 @@ var Answerbutton = React.createClass({
 var Rbutton =React.createClass({
   render: function(){
     return (
-      <div className="btn-r" class="btn-group btn-group-justified" role="group" aria-label="...">
+      <div className="btn-group btn-group-justified" role="group" aria-label="...">
         <Trybutton tryurl={this.props.tryurl}/>
         <Answerbutton ansurl={this.props.ansurl}/>
       </div>
@@ -33,7 +33,7 @@ var makerbutton=function(urls){
     console.log("try:"+tryurl);
     var ansurl="http://runnable.com/"+urls[i].ans;
     console.log("ans:"+ansurl);
-    var mybutton=<Rbutton tryurl={tryurl } ansurl={ansurl} />;
+    var mybutton=<Rbutton tryurl={tryurl} ansurl={ansurl} />;
     React.render(mybutton,document.getElementById(urls[i].id));
   }
 }
