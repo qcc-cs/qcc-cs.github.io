382dd42546a55d3fdadd8210041d79a3a10bea1d
kwangkim <kkim@qcc.cuny.edu>
2015-01-28 06:50:08 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -1,11 +1,10 @@
-function updatetab(url, pane, json) {
+function updatetab(url, pane) {
       // From http://stackoverflow.com/a/651735
       var ext = url.split('.').pop().toLowerCase();
       var ismd = true;
       if ($.inArray(ext, ['md', 'markdown', 'mdown', 'mkdn', 'mkd', 'mdtxt', 'mdtext']) == -1) {
         ismd = false;
       }
- 
       $.get(url, function(data) {
             if (ismd) {
                   $('#my-pagination-content').html(marked(data));
@@ -14,26 +13,9 @@ function updatetab(url, pane, json) {
                   $('#my-pagination-content').html(data);
                   MathJax.Hub.Queue(["Typeset", MathJax.Hub, "my-pagination-content"]);
             }
-            if (json !== "lectures/") {
-                  console.log("run makerbutton");
-                  makerbutton(lecbuttons);
-            }
-        /*
-        console.log("json1:"+json);
-        if (json !== "lectures/") {
-          console.log("json2:"+json);
-          $.ajax({
-            url: json,
-            dataType: "jsonp",
-            jsonpCallback: "a/b:c",
-            success: function(data) {
-              console("data:"+data);
-              makerbutton(data);
-            }
-          })
-        }*/
-      });
-
+           
+            makerbutton();
+      }
       pane.tab('show');
 }
 
