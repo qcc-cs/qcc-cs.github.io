79eb826fce6f490483ae4760a2a01a815f41e2dc
kwangkim <kkim@qcc.cuny.edu>
2015-02-06 17:42:07 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -76,8 +76,8 @@ var Latexdown={
   Escape: function (html, encode) {
     return html
       .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
-      //.replace(/</g, '&lt;')
-      //.replace(/>/g, '&gt;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
       .replace(/"/g, '&quot;')
      .replace(/'/g, '&#39;');
   }
